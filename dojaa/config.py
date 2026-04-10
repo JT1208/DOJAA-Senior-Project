d12738115f6d65b55478ddae21b6148a60202a0a
ogs23 <shitalsavaliya@Shitals-MacBook-Air.local>
2026-04-10 13:34:51 -0400

    jon api

--- dojaa/config.py
+++ dojaa/config.py
@@ -1,6 +1,9 @@
 # dojaa/config.py
 
-SHODAN_API_KEY = "2AvEeVmSRvSMYCNI2iBLfDA5NNWAlidY"
+#om's API key
+#SHODAN_API_KEY = "2AvEeVmSRvSMYCNI2iBLfDA5NNWAlidY"
+# jon's API key
+SHODAN_API_KEY = "qBA6erhzKJWy2L51g0FjgbYo4PI2vYwD"
 SHODAN_QUERY = "hostname:drexel.edu"  # replace with your organization filter
 CENSYS_API_TOKEN = "censys_9HAR5SGh_4YAvggoFqmQqf2czxRffiDm4"
 CENSYS_QUERY = "services.service_name: HTTP"
